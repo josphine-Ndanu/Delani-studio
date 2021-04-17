1955bf646cf2c8128083ecb2b12f7707974dfbb9
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-04-17 14:33:55 +0300

    What we do complete, toggle working

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+    $(".design").click(function () {
+        $(".design-img").toggle();
+        $(".design-desc").toggle();
+    });
+
+    $(".dev").click(function () {
+        $(".dev-img").toggle();
+        $(".dev-desc").toggle();
+    });
+
+    $(".productMg").click(function () {
+        $(".produc-img").toggle();
+        $(".produc-desc ").toggle();
+    });
+
