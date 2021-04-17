1677176ab0a7e63a6935a94e620e4170de1fef0f
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-04-17 23:54:06 +0300

    Portfolio hover effect complete

--- js/script.js
+++ js/script.js
@@ -1,22 +1,83 @@
+$(".design").click(function () {
+    $(".design-img").toggle();
+    $(".design-desc").toggle();
+});
 
-    $(".design").click(function () {
-        $(".design-img").toggle();
-        $(".design-desc").toggle();
-    });
+$(".dev").click(function () {
+    $(".dev-img").toggle();
+    $(".dev-desc").toggle();
+});
 
-    $(".dev").click(function () {
-        $(".dev-img").toggle();
-        $(".dev-desc").toggle();
-    });
+$(".productMg").click(function () {
+    $(".produc-img").toggle();
+    $(".produc-desc ").toggle();
+});
 
-    $(".productMg").click(function () {
-        $(".produc-img").toggle();
-        $(".produc-desc ").toggle();
-    });
+//  $(".prjct2","".prjct3",.prjct1,.prjct4,.prjct5,.prjct6,.prjct7,.prjct8").click(function () {
+//     $(".overlay").show();
+// });
+// $("#
+// prjct1").mouseenter(function () {
+//     $(".overlay").show();
+// });
 
-    $(".prjcts").click(function () {
-        $(".overlay").show();
-    });
-    // $("#portfolio2").click(function () {
-    //     $(".overlay2").show();
-    // });
+//  var projects =["#prjct1","#prjct2","#prjct3","#prjct4","#prjct5","#prjct6","#prjct7","#prjct8"]
+//  projects.forEach(element => {
+//      $("project").mouseenter(function () {
+//         $(".overlay").show();
+//      })
+//  });
+
+$("#prjct1").mouseover(function () {
+
+    $(" .pone").show();
+}).mouseout(function () {
+    $(" .pone").hide();
+})
+
+$("#prjct2").mouseover(function () {
+
+    $(" .ptwo").show();
+}).mouseout(function () {
+    $(" .ptwo").hide();
+})
+
+$("#prjct3").mouseover(function () {
+
+    $(" .pthree").show();
+}).mouseout(function () {
+    $(" .pthree").hide();
+})
+
+$("#prjct4").mouseover(function () {
+
+    $(" .pfour").show();
+}).mouseout(function () {
+    $(" .pfour").hide();
+})
+
+$("#prjct5").mouseover(function () {
+
+    $(" .pfive").show();
+}).mouseout(function () {
+    $(" .pfive").hide();
+})
+
+$("#prjct6").mouseover(function () {
+
+    $(" .psix").show();
+}).mouseout(function () {
+    $(" .psix").hide();
+})
+$("#prjct7").mouseover(function () {
+
+    $(" .pseven").show();
+}).mouseout(function () {
+    $(" .pseven").hide();
+})
+$("#prjct8").mouseover(function () {
+
+    $(" .peight").show();
+}).mouseout(function () {
+    $(" .peight").hide();
+})
