4713a8e2cba20585b58f1e328fc75e9fa65f68ae
josphine-Ndanu <josephine.nguli@student.moringaschool.com>
2021-04-19 00:57:56 +0300

    js and mailchimp

--- js/script.js
+++ js/script.js
@@ -94,9 +94,9 @@ $("#prjct8").mouseover(function () {
 //       else {
 //         alert("Please enter a valid email and name!");
 //       }
-      
+
 //     });
-  
+
 //   });
 
 // (function ($) {
@@ -110,18 +110,18 @@ $("#prjct8").mouseover(function () {
 //                 ftypes[2] = 'text';
 //             }(jQuery));
 //             var $mcj = jQuery.noConflict(true);
-     
-           
-$("form#sub").submit(function(event){
-    
-          var name = $("input#mce-NAME").val();
-          var email = $("input#mce-EMAIL").val();
-          var message = $("textarea#msg").val();
-          if ($("input#mce-NAME").val() && $("input#EMAIL").val()){
-            alert (name + ",Delani Studio  has recieve you message.We will get back to you soon.Keep it Delani studio always. Thanks for your feedback.:)");
-          }
-          else {
-            alert("Please enter a valid email and name!");
-          }
-          
-        });
+
+
+$(".submitInfo").click(function (event) {
+
+    event.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#msg").val();
+    if ($("#name").val("") && $("#msg").val("")) {
+        alert(name + "Delani studio, We appreciate your feedback.:)");
+    } else {
+        alert("Please fill all fields!");
+    }
+
+});
